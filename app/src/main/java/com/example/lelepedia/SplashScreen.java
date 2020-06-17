1d2f5303b2f25c09397fccd8053692c7e6f3a867
hafidhfajar <35284076+hafidhfajar@users.noreply.github.com>
2020-06-17 13:30:01 +0700

    commit 3

--- app/src/main/java/com/example/lelepedia/SplashScreen.java
+++ app/src/main/java/com/example/lelepedia/SplashScreen.java
@@ -3,24 +3,45 @@ package com.example.lelepedia;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends Activity {
 
+    private static int SPLASH_SCREEN = 3500;
+
+    //variable
+    Animation topAnim,botAnim;
+    ImageView image;
+    TextView name,slogan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splach_screen);
-        Thread thread = new Thread() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        //animasi
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        botAnim = AnimationUtils.loadAnimation(this,R.anim.bot_animation);
+
+        //hooks
+        image = findViewById(R.id.imageView);
+        name = findViewById(R.id.textView);
+
+        image.setAnimation(topAnim);
+        name.setAnimation(botAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
             public void run() {
-                try {
-                    sleep(4000);
-                }   catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    startActivity(new Intent(SplashScreen.this, Home.class));
-                }
+                Intent intent = new Intent(SplashScreen.this, Home.class);
+                startActivity(intent);
+                finish();
             }
-        };
-        thread.start();
+        },SPLASH_SCREEN);
     }
 }
