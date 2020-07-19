8fa7b8897bf6326d05e5bd733cba3fc7ab8c57e4
hafidhfajar <35284076+hafidhfajar@users.noreply.github.com>
2020-07-19 15:27:02 +0700

    19 Juli

--- app/src/main/java/com/example/lelepedia/Valorant/FragmentTransfer.java
+++ app/src/main/java/com/example/lelepedia/Valorant/FragmentTransfer.java
@@ -10,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.lelepedia.R;
-import com.example.lelepedia.R6.FragTransfer;
 
 public class FragmentTransfer extends Fragment {
 
